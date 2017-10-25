8451509d6b7efe5354aa95d721b7b0c2608dd6c6
G Naga Subrahmanyam <gnsubrahmanyam1@gmail.com>
2017-10-25 13:09:02 +0530

    Minor changes

--- src/Main.purs
+++ src/Main.purs
@@ -4,7 +4,13 @@ import Prelude
 
 import Control.Monad.Eff (Eff)
 import Data.Maybe (Maybe(..))
-import Graphics.Canvas (CANVAS, arc, closePath, beginPath, fillPath, getCanvasElementById, getContext2D, stroke, lineTo, moveTo, rect, setFillStyle, setStrokeStyle, setCanvasWidth, setCanvasHeight, getCanvasWidth, getCanvasHeight)
+import Graphics.Canvas (CANVAS,
+  arc, closePath,
+  beginPath, fillPath,
+  getCanvasElementById, getContext2D,
+  stroke, lineTo,
+  moveTo, rect,
+  setFillStyle, setStrokeStyle)
 import Math
 import Partial.Unsafe (unsafePartial)
 
@@ -13,12 +19,7 @@ main :: Eff (canvas :: CANVAS) Unit
 main = void $ unsafePartial do
   Just canvas <- getCanvasElementById "canvas"
   ctx <- getContext2D canvas
-
-  w <- getCanvasWidth canvas
-  h <- getCanvasHeight canvas
-  _ <- setCanvasWidth w canvas
-  _ <- setCanvasHeight h canvas
-
+  
   _ <- setFillStyle "#0099FF" ctx
   _ <- setStrokeStyle "#0099FF" ctx
 
